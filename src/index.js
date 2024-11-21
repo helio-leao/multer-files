c1c825513cba1ebb214d9e14ced720783290e886
Helio Leao <hmtleao@hotmail.com>
2024-11-20 21:33:50 -0300

    add delete file route

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import express from "express";
 import diskStorageRouter from "./routes/diskStorage.js";
 import memoryStorageRouter from "./routes/memoryStorage.js";
 import fs from "fs/promises";
-import { FILES_DIRECTORY } from "./constants/fileConstants.js";
 import path from "path";
+import { FILES_DIRECTORY } from "./constants/fileConstants.js";
+
+const __dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -18,8 +20,7 @@ app.get("/", async (_req, res) => {
   try {
     const files = await fs.readdir(FILES_DIRECTORY);
     res.json(files);
-  } catch (error) {
-    console.error(error);
+  } catch {
     res.sendStatus(500);
   }
 });
@@ -28,10 +29,22 @@ app.get("/", async (_req, res) => {
 app.get("/:fileName", async (req, res) => {
   const { fileName } = req.params;
 
-  if (fileName) {
-    res.sendFile(path.join(path.resolve(), FILES_DIRECTORY, fileName));
-  } else {
-    res.sendStatus(404);
+  try {
+    res.sendFile(path.join(__dirname, FILES_DIRECTORY, fileName));
+  } catch {
+    res.sendStatus(500);
+  }
+});
+
+// delete a file by name
+app.delete("/:fileName", async (req, res) => {
+  const { fileName } = req.params;
+
+  try {
+    await fs.unlink(path.join(__dirname, FILES_DIRECTORY, fileName));
+    res.sendStatus(204);
+  } catch {
+    res.sendStatus(500);
   }
 });
 
